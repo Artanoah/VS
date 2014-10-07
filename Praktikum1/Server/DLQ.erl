bd4220f25ae7d819a1009a6777dbc9624e30caf8
Artanoah <artanoah@web.de>
2014-10-07 15:49:52 +0400

    Weiter

--- Praktikum1/Server/DLQ.erl
+++ Praktikum1/Server/DLQ.erl
@@ -1,8 +1,21 @@
 -module(dlq).
--export([createNew/0]).
+-export([create_new/0, add/3]).
 
-createNew() -> 
+create_new() -> 
 	[].
 	
 add(Msg, Nr, Queue) ->
+	case full(Queue) of
+		true	-> 	lists:keysort(2, [{Msg, Nr}] ++ delete_first(Queue));
+		false 	->	lists:keysort(2, [{Msg, Nr}] ++ Queue)
+	end.
+		
+		
+full(Queue) ->
+	second_entry(application:get_env(server, dlq_max_size)) =< length(Queue).
 	
+second_entry({_, Val}) ->
+	Val.
+	
+delete_first([_X|XS]) ->
+	XS.
---
+++ Praktikum1/Server/server.app
@@ -0,0 +1,5 @@
+{application, server,
+ [{description, "Server for a Message Broadcasting-System"},
+  {vsn, "1"},
+  {env, [{dlq_max_size, 2}]}
+]}.
