d7bca2a8b7e73f8c7bd98b60eb39bbfc9074bc14
Artanoah <artanoah@web.de>
2014-10-08 15:31:28 +0200

    Dinge

--- Praktikum1/Server/DLQ.erl
+++ Praktikum1/Server/DLQ.erl
@@ -1,5 +1,5 @@
 -module(dlq).
--export([create_new/0, add/3]).
+-export([create_new/0, add/3, get/2]).
 
 create_new() -> 
 	[].
@@ -9,6 +9,20 @@ add(Msg, Nr, Queue) ->
 		true	-> 	lists:keysort(2, [{Msg, Nr}] ++ delete_first(Queue));
 		false 	->	lists:keysort(2, [{Msg, Nr}] ++ Queue)
 	end.
+
+
+get(_, []) ->
+	false;
+
+get(Nr, [{_, NNr} | XS]) when Nr /= NNr ->
+	get(Nr, XS);
+
+get(Nr, [{Nachricht, _} | XS]) when XS /= [] ->
+	{Nachricht, Nr, true};
+
+get(Nr, [{Nachricht, _} | _]) ->
+	{Nachricht, Nr, false}.
+	
 		
 		
 full(Queue) ->
@@ -17,5 +31,5 @@ full(Queue) ->
 second_entry({_, Val}) ->
 	Val.
 	
-delete_first([_X|XS]) ->
+delete_first([_|XS]) ->
 	XS.
