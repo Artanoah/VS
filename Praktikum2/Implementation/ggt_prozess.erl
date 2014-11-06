66519cfba86dd99c57bf52826f2c9f0282a6a9f9
Artanoah <artanoah@web.de>
2014-11-06 14:12:50 +0100

    Dinge

--- Praktikum2/Implementation/ggt_prozess.erl
+++ Praktikum2/Implementation/ggt_prozess.erl
@@ -19,12 +19,19 @@ start(Arbeitszeit, Wartezeit, Name) ->
 	% Versuche den eigenen Namen beim Nameservice zu binden
 	case util:bind_name(Nameservice, Name) of
 		ok -> 
-			util:logging("GGT.log", "Name " ++ atom_to_list(Name) ++ " erfolgreich registriert");
+			util:logging("GGT.log", "Name " ++ atom_to_list(Name) ++ " erfolgreich registriert\n");
 		in_use -> 
-			util:logging("GGT.log", "Name " ++ atom_to_list(Name) ++ " schon in benutzung - Beende Prozess"),
-			error
+			util:logging("GGT.log", "Name " ++ atom_to_list(Name) ++ " schon in benutzung - Beende Prozess\n"),
+			erlang:fault("Name bereits vergeben\n")
 	end,
-
+	
+	% Melde dich beim Koordinator an
+	util:lookup_name(Nameservice, KoordinatorName) ! {hello, Name},
+	
+	% Warte auf eine erste Nachricht
+	wait_for_first_message(),
+	
+	% GGT-Prozessloop
 	loop(Arbeitszeit, Wartezeit, Name, KoordinatorName, Nameservice, -1, -1, -1, util:time_in_ms()),
 
 	% Melde beim Nameservice ab
@@ -39,27 +46,28 @@ loop(Arbeitszeit, Wartezeit, Name, KoordinatorName, Nameservice, Mi, LeftNeighbo
 		{sendy, Y} ->
 			NewMi = euklid(Mi, Y),
 			if
-				NewMi == Mi ->
+				NewMi >= Mi ->
 					loop(Arbeitszeit, Wartezeit, Name, KoordinatorName, Nameservice, Mi, LeftNeighbor, RightNeighbor, util:time_in_ms());
-				NewMi =/= Mi ->
+				NewMi < Mi ->
 					util:lookup_name(Nameservice, LeftNeighbor) ! {sendy, NewMi},
 					util:lookup_name(Nameservice, RightNeighbor) ! {sendy, NewMi},
-					loop(Arbeitszeit, Wartezeit, Name, KoordinatorName, Nameservice, Mi, LeftNeighbor, RightNeighbor, util:time_in_ms())
+					loop(Arbeitszeit, Wartezeit, Name, KoordinatorName, Nameservice, NewMi, LeftNeighbor, RightNeighbor, util:time_in_ms())
 			end;
 		{abstimmung, Initiator} ->
 			case Name == Initiator of
 				true ->
-					util:lookup_name(Nameservice, Koordinator) ! {briefterm, {Name, Mi, util:timeMilliSecond()}},
+					util:lookup_name(Nameservice, KoordinatorName) ! {briefterm, {Name, Mi, util:timeMilliSecond()}},
+					wait_for_first_message(),
 					loop(Arbeitszeit, Wartezeit, Name, KoordinatorName, Nameservice, Mi, LeftNeighbor, RightNeighbor, util:time_in_ms());
 				false ->
-					case ()util:time_in_ms() - Timestamp) > ()Wartezeit / 2) of
+					case (util:time_in_ms() - Timestamp) > (Wartezeit / 2) of
 						true ->
-							util:lookup_name(Nameservice, RightNeighbor) ! {abstimmung, Initiator};
-						false ->
+							util:lookup_name(Nameservice, RightNeighbor) ! {abstimmung, Initiator},
 							loop(Arbeitszeit, Wartezeit, Name, KoordinatorName, Nameservice, Mi, LeftNeighbor, RightNeighbor, util:time_in_ms());
-					end;
-			end,
-			
+						false ->
+							loop(Arbeitszeit, Wartezeit, Name, KoordinatorName, Nameservice, Mi, LeftNeighbor, RightNeighbor, util:time_in_ms())
+					end
+			end;
 			
 		{tellmi, From} ->
 			util:lookup_name(Nameservice, From) ! {mi, Mi},
@@ -70,7 +78,8 @@ loop(Arbeitszeit, Wartezeit, Name, KoordinatorName, Nameservice, Mi, LeftNeighbo
 		kill -> ok
 	after 
 		Wartezeit ->
-			util:lookup_name(Nameservice, RightNeighbor) ! {abstimmung, Name}
+			util:lookup_name(Nameservice, RightNeighbor) ! {abstimmung, Name},
+			loop(Arbeitszeit, Wartezeit, Name, KoordinatorName, Nameservice, Mi, LeftNeighbor, RightNeighbor, util:time_in_ms())
 	end.
 
 
@@ -84,4 +93,18 @@ wait_for_nameservice(NameserviceNode) ->
 			wait_for_nameservice(NameserviceNode);
 		Answer == pong ->
 			ok
+	end.
+
+euklid(Mi, Y) ->
+	if
+		Y < Mi ->
+			((Mi - 1) rem Y) + 1;
+		true -> 
+			Mi
+	end.
+
+wait_for_first_message() ->
+	receive 
+		Message ->
+			self() ! Message
 	end.
